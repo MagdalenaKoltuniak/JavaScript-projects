b2ab566cec7e730d55e3f146d8241f25baeaa0a5
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2025-08-28 17:34:40 +0200

    added Project6_ex2 code generator

---
+++ Project6_ex2/main.js
@@ -0,0 +1,22 @@
+const chars = 'ABCDEFGHIJKLMNOPRSTUWZXQ123456789';
+
+const btn = document.querySelector('button');
+const section = document.querySelector('section');
+
+const codesNumber = 1000;
+const charsNumber = 10;
+
+const codesGenerator = () => {
+	for (let i = 0; i < codesNumber; i++) {
+		let code = '';
+		for (let j = 0; j < charsNumber; j++) {
+			const index = Math.floor(Math.random() * chars.length);
+			code += chars[index];
+		}
+		const div = document.createElement('div');
+		div.textContent = code;
+		section.appendChild(div);
+	}
+};
+
+btn.addEventListener('click', codesGenerator);
